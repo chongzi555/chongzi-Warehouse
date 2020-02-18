c3b2a8c711a7303c2a174f8038e79ce7b457458c
chongzi <162717782@qq.com>
2020-02-18 14:58:04 +0800

    no message

--- node/node/Models/good_ware.js
+++ node/node/Models/good_ware.js
@@ -2,6 +2,6 @@ const { db } = require('../Schema/config');
 
 const GoodWareSchema = require('../Schema/good_ware');
 
-const Good = db.model('goods', GoodWareSchema);
+const Good = db.model('ware_goods', GoodWareSchema);
 
 module.exports = Good;
